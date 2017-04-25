259359e6c9a024bd0a2221db39d9894263ace1db
shin <hg>
2017-04-25 16:41:54 +0900

    Change: コンテナを使ってループ回数を表示

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-
+#include <vector>
 int main(int argc, char* argv[]) {
+
+	std::vector<int> p;
 
 	for(auto i=0; i<=10; i++) {
 		std:: cout << "Hello, World!" << std::endl;
+		p.push_back(i);
 	}
+
+	for(auto j : p)
+		std::cout << j << std::endl;
 
 	std::cin.get();
 	std::cin.get();
